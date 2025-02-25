f46bfa5910774d080beca888b6e4696827876968
Keith Estrella <keithestrella@gmail.com>
2025-02-25 03:00:35 -0500

    Add form submission

--- script.js
+++ script.js
@@ -10,13 +10,34 @@ function Book(title, author, pages, haveRead) {
 function addBookToLibrary(title, author, pages, haveRead) {
     let book = new Book(title, author, pages, haveRead);
 
-    myLibrary.push(myLibrary);
+    myLibrary.push(book);
+
+
+    const iterator = myLibrary.values();
+
+    for (let value of iterator) {
+        console.log(value);
+    }
 }
 
 // For the form
 const openForm = document.getElementById("new-book");
-const form = document.getElementById("book-form");
+const formSection = document.getElementById("book-form");
+const form = document.getElementById("form");
 
 openForm.addEventListener('click', () => {
-    form.style.display = 'block';
+    formSection.style.display = 'block';
 })
+
+function handleFormSubmit(event) {
+    event.preventDefault();
+
+    const bookTitle = document.getElementById("book-title").value;
+    const bookAuthor = document.getElementById("book-author").value;
+    const bookPages = document.getElementById("book-pages").value;
+    const bookRead = document.getElementById("book-read").checked;
+
+    addBookToLibrary(bookTitle, bookAuthor, bookPages, bookRead);
+}
+
+form.addEventListener("submit", handleFormSubmit);
